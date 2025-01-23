6a905dcf8087d6e95e15a7057d505fa7e16f9267
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-23 15:44:51 -0300

    Drawer dos produtos do carrinho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
 import Navbar from './components/Navbar'
 import { ClerkProvider } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Next E-commerce 15",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,14 +35,14 @@ export default async function Home() {
   console.log(products);
 
 
-
   return (
 
-    <div className="max-w-7x1 mx-auto pt-8 px-8 xl:px-0  ">
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0  ">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 xl:gap-6 ">
         {products.map((product) => (
           <Product key={product.id} product={product}></Product>
         ))}
+        euu
 
       </div>
     </div>
