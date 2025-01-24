2231a6d2536f696984d17e1ae5a2814f6402457f
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-23 23:16:30 -0300

    adicionado logica de adicionar ao carrinho

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ import { ProductType } from "@/types/ProductType";
 
 type CartState = {
     cart: ProductType[];
-    // addProduct: (product: ProductType) => void;
+    addProduct: (product: ProductType) => void;
     // removeProduct: (productId: string) => void;
     isOpen: boolean;
     toggleCart: () => void;
@@ -14,6 +14,23 @@ export const useCartStore = create<CartState>()(
     persist(
         (set) => ({
             cart: [],
+            addProduct: (item) =>
+                set((state) => {
+                    const product = state.cart.find((p) => p.id === item.id);
+
+                    if (product) {
+                        const updatedCart = state.cart.map((p) => {
+                            if (p.id === product.id) {
+                                return { ...p, quantity: p.quantity ? p.quantity + 1 : 1 };
+                            }
+                            return p
+                        });
+                        return { cart: updatedCart }
+                    } else {
+                        return { cart: [...state.cart, { ...item, quantity: 1}]}
+
+                    }
+                }),
             isOpen: false,
             toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
         }),
